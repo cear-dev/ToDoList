03801fc0c8d662a857923ff243ff70b10a1b2b1f
cear-dev <carloalvarez318@gmail.com>
2024-05-22 20:05:41 -0600

    Initial commit

---
+++ TDMPW_411_3P_PR02/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TDMPW_411_3P_PR02;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TDMPW_411_3P_PR02/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TDMPW_411_3P_PR02;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
